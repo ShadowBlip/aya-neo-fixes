cc4bc8202f01113c2901682dc8d4c48ea3b40a40
Derek J. Clark <derekjohn.clark@gmail.com>
2022-04-26 16:41:02 -0700

    Added retroarch configuration for the virtual controller.

--- install.sh
+++ install.sh
@@ -13,8 +13,9 @@ echo "Adding suspend loss of wifi on suspend fix"
 cp -v systemd-suspend-mods.conf /etc/systemd-suspend-mods.conf
 cp -v systemd-suspend-mods.sh /usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
 
-echo "Enabling unmapped buttons. You will need to manually configure the new controller device in steam."
+echo "Enabling unmapped buttons. NEXT users will need to configure the Home button in steam."
 cp -v neo-controller.py /usr/local/bin/
 cp -v neo-controller.service /etc/systemd/system
+cp -v Aya-Neo-Controller.cfg /usr/share/libretro/autoconfig/udev # retroarch config file
 systemctl enable neo-controller && systemctl start neo-controller
 exit 0
