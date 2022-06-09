cbd0def54979bee198fbb0bfa69d436de9ab5078
Derek J. Clark <derekjohn.clark@gmail.com>
2022-06-08 20:14:32 -0700

    Added bmi160 fix.

--- install.sh
+++ install.sh
@@ -8,10 +8,11 @@ echo "Adding suspend loss of WiFi on suspend fix"
 cp -v systemd-suspend-mods.conf /etc/systemd-suspend-mods.conf
 cp -v systemd-suspend-mods.sh /usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
 
-echo "Enabling unmapped buttons. NEXT users will need to configure the Home button in steam."
+echo "Enabling controller functionality. NEXT users will need to configure the Home button in steam."
 cp -v neo-controller.py /usr/local/bin/
-cp -v neo-controller.service /etc/systemd/system
+cp -v neo-controller.service /etc/systemd/system/
 cp -v 60-neo-controller.rules /etc/udev/rules.d/
+cp -v bmi160_i2c.conf /etc/modules-load.d/
 systemctl enable neo-controller && systemctl start neo-controller
 
 exit 0
--- remove.sh
+++ remove.sh
@@ -8,10 +8,11 @@ echo "Removing loss of WiFi on suspend fix."
 rm -v /etc/systemd-suspend-mods.conf
 rm /usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
 
-echo "Disabling unmapped buttons."
+echo "Disabling extra controller functions."
 systemctl stop neo-controller && systemctl disable neo-controller
 rm -v /etc/systemd/system/neo-controller.service
 rm -v /usr/local/bin/neo-controller.py
 rm -v /etc/udev/rules.d/60-neo-controller.rules 
+rm -v /etc/modules-load.d/bmi160_i2c.conf
 
 exit 0
